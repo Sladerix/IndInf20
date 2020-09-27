73922b43e9c8f0d5506678ad5a942afc79a54ca9
Johanz9 <johanz147@gmail.com>
2020-09-27 13:57:09 +0200

    Modificato automata-1-Mendoza, ora puo accettare l'argomento dalla linea di comando, poi il codice è stato ristrutturato aggiungendo la funzione nextstate

--- Code/Johan_Mendoza/automata-1-Mendoza.py
+++ Code/Johan_Mendoza/automata-1-Mendoza.py
@@ -1,31 +1,66 @@
+"""
+  Computes next state. Returns the following
 
+    -1 : input sequence not legal
+    -2 : we're in final state
 """
-L'ho fatto su python, ma successivamente lo faro in C++ e lo carichero.
-"""
+
+import sys
+
+def nextstate(current_state,inputs):
+
+    if inputs == "a" and current_state == "S0":
+        current_state = "S2"
+    elif inputs == "a" and current_state == "S2":
+        current_state = "S0"
+    elif inputs == "b" and current_state == "S0":
+        current_state = "S1"
+    elif inputs == "b" and current_state == "S1":
+        current_state = "S1"
+    elif inputs == "c" and current_state == "S0":
+        current_state = "S3"
+        return -2
+    elif inputs == "c" and current_state == "S1":
+        current_state = "S3"
+        return -2
+    else:
+        return -1
+    return current_state
 
 if __name__ == '__main__':
-    #inputsize = 4
-    inputs = ['a','a','b','c']
-    stato = "S0" #di default
-    print("Stato iniziale:",stato)
+    #print(sys.argv[1])
+
+    #get input from command line
+    if len(sys.argv) < 2:
+        print("Error, insert an input")
+        sys.exit()
+    elif len(sys.argv) > 2:
+        print("Error, expected only 1 argoment/array")
+        sys.exit()
+    else:
+        inputs = list(map(str, sys.argv[1].rstrip().split(",")))
+
+    #inputs = ['a','a','b','c']
+
+    current_state = "S0" #di default
+
     for i in inputs:
-        if i == "a" and stato == "S0":
-            stato = "S2"
-        elif i == "a" and stato == "S2":
-            stato = "S0"
-        elif i == "b" and stato == "S0":
-            stato = "S1"
-        elif i == "b" and stato == "S1":
-            stato = "S1"
-        elif i == "c" and stato == "S0":
-            stato = "S3"
+
+        print("Current state:", current_state)
+
+        stato = nextstate(current_state,i)
+
+        if stato == -1:
+            print("input sequence ({inputs}) not legal for state {stato}".format(inputs=i, stato=current_state))
             break
-        elif i == "c" and stato == "S1":
-            stato = "S3"
+
+        elif stato == -2:
+            print("we're in final state: S3")
             break
         else:
-            print("Error")
-            break
-        print("Stato attuale:",stato)
-    print("Stato finale:",stato)
+
+            print("Next state:",stato)
+
+            current_state = stato
+
     print("Done")
