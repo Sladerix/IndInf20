6a3760878be5f4a50a4c8a85df2a7086fd17ecf4
banana <cesco.moretti@gmail.com>
2020-09-29 09:12:28 +0200

    aggiunta macchina moore-mealy

---
+++ Code/francesco_moretti_130301/macchina di moore-mealy.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+//this finite state machine takes in input an array of the letter a, b, c 
+const char inputs[] = {'a', 'a', 'b', 'b', 'b', 'c'};
+
+
+
+const int n_input = 3; //number of the different possible input
+const int n_stati = 4; //number of the state
+struct str_state
+{
+    int values[n_input];
+    int output;
+};
+
+                                   //{ a  b  c} output
+const str_state table[n_stati] = {  {{ 2, 1, 3},4}, //s0
+                                    {{-1, 1, 3},7}, //s1                                    
+                                    {{ 0,-1,-1},3}, //s2
+                                    {{-1,-1,-1},5}};//s3
+
+char next(){
+    static int i =0;
+    return inputs[i++];
+}
+int output(int n){
+    return table[n].output;
+}
+
+int main () {   
+    int state = 0;
+    char curr_input;
+    int n = sizeof(inputs) / sizeof(inputs[0]);  //this finds the size of the array 
+    cout<< output(state)<<endl; //print the output of the starting state
+
+    for(int k=0;k<n;k++){ 
+        curr_input = next();
+        int index = (int) curr_input - 'a';
+        if(index >= 3){ //check if the input is not a, b or c -->default case
+            cout<<"input non esistente"<<endl;
+            goto error;
+        }             
+        int nextstate = table[state].values[index];
+        if (nextstate < 0){ //check the combination of the current state and the input is correct
+            cout<<"input non consentito nello stato s"<< state <<endl;
+            goto error;            
+        }
+        else{
+            if(state != nextstate){ //print the output if the state is changed
+                cout<< output(nextstate)<<endl;
+            } 
+            state=nextstate;
+            /*macchina mealy
+             if(state == nextstate){
+                cout<<"stato rimasto a s"<<state<<enl;
+            }else{
+                cout<<"stato cambiato a s"<<state<<endl; 
+            } */            
+        }        
+    }        
+    cout <<"sequenza legale"<<endl;
+    return 0;
+
+    error:
+    cout <<"Errore"<<endl;
+    return -1;  
+}
