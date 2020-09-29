2df47caf9e19f29e389c799080833d0e2e325569
banana <cesco.moretti@gmail.com>
2020-09-29 11:54:58 +0200

    corretto errore

--- Code/francesco_moretti_130301/macchina di moore.mealy.cpp
+++ Code/francesco_moretti_130301/macchina di moore.mealy.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 
 //this finite state machine takes in input an array of the letter a, b, c 
-const char inputs[] = {'a', 'a', 'b', 'b', 'b', 'c'};
+const char inputs[] = {'a', 'a', 'b', 'b', 'c'};
 
 
 
@@ -35,7 +35,7 @@ int main () {
     int state = 0;
     char curr_input;
     int n = sizeof(inputs) / sizeof(inputs[0]);  //this finds the size of the array 
-    cout<< output(state)<<endl; //print the output of the starting state
+    cout<< output(state)<<endl; //moore -->print the output of the starting state
 
     for(int k=0;k<n;k++){ 
         curr_input = next();
@@ -43,23 +43,16 @@ int main () {
         if(index >= 3){ //check if the input is not a, b or c -->default case
             cout<<"input non esistente"<<endl;
             goto error;
-        }             
+        }
+        //cout<<"input ricevuto"<<endl;  mealy --> print if an input is received            
         int nextstate = table[state].values[index];
         if (nextstate < 0){ //check the combination of the current state and the input is correct
             cout<<"input non consentito nello stato s"<< state <<endl;
             goto error;            
         }
         else{
-            if(state != nextstate){ //print the output if the state is changed
-                cout<< output(nextstate)<<endl;
-            } 
-            state=nextstate;
-            /*macchina mealy
-             if(state == nextstate){
-                cout<<"stato rimasto a s"<<state<<enl;
-            }else{
-                cout<<"stato cambiato a s"<<state<<endl; 
-            } */            
+            cout<< output(nextstate)<<endl; //moore -->print the output of the state (exept the starting one)        
+            state=nextstate;                       
         }        
     }        
     cout <<"sequenza legale"<<endl;
