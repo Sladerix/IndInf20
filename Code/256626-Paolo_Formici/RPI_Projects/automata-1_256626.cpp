2e3b1a1baf839a8b0b2f9b6c550e84c62d042287
Paolo Formici <pformici@gmail.com>
2020-10-08 16:24:47 +0200

    Updated code structure

--- Code/256626-Paolo_Formici/RPI_Projects/automata-1_256626.cpp
+++ Code/256626-Paolo_Formici/RPI_Projects/automata-1_256626.cpp
@@ -5,6 +5,7 @@
 #include <map>
 #include <unistd.h>
 
+
 #ifndef NO_PI
     #include <wiringPi.h>
 #else  /// if not included with wiringPi headers, define them
@@ -14,7 +15,6 @@
 
 using namespace std;
 
-
 ///
 /// Configuration
 ///
@@ -22,6 +22,7 @@ using namespace std;
 #define LED_GREEN 1
 #define LED_BLUE 2
 #define LED_YELLOW 3
+
 
 ///
 /// Utils
@@ -36,6 +37,7 @@ void init() {
     #endif
 }   
 
+/* Sets ledNumber to value [HIGH - LOW] */
 void setLed(int ledNumber, bool value) {
 #ifndef NO_PI
     digitalWrite(ledNumber, value);
@@ -44,43 +46,67 @@ void setLed(int ledNumber, bool value) {
 #endif
 }
 
-/* Final state not reached error:
-   - clear mid-state led indicator
-   - set error led indicator
-*/
-void simpleError() {
-    // No need to switch off initial state led because input string lenght supposed to be > 0, else if not legal input at state 0 switch to error -2
-    setLed(LED_YELLOW, LOW);
-    setLed(LED_RED, HIGH);
-}
-
-/* Input sequence not legal error:
-   - clear initial-state led indicator
-   - clear mid-state led indicator
-   - set error led indicator to blinking
-*/
-void blinkError() {
-    setLed(LED_GREEN, LOW);
-    setLed(LED_YELLOW, LOW);
-    // Setting red LED to blinking
+/* Sets ledNumber to blink 5 times, then on */
+void blinkLed(int ledNumber) {
     bool onoff = true;
-    while(true) {
-        setLed(LED_RED, onoff);
+    int times = 0;
+    while(times != 5) {
+        setLed(ledNumber, onoff);
         onoff = !onoff;
         #ifndef NO_PI
             delay(1000);
         #else
             sleep(1);
         #endif
+        times++;
     }
 }
 
-/* Gets input char, or 'x' if no char left */
+/* Switch all ledNumbers to LOW */
+void clearAllLed() {
+    setLed(LED_RED, LOW);
+    setLed(LED_GREEN, LOW);
+    setLed(LED_BLUE, LOW);
+    setLed(LED_YELLOW, LOW);
+}
+
+
+/* Gets input char, or '-' if no char left */
 char next(string word) {
     static int i = 0;
     if(i == word.length())
-        return 'x';
+        return '-';
     return word[i++];
+}
+
+/* Updates output depending on the state:
+   - case 0: Initial state, LED_GREEN HIGH ;
+   - case 3: Final state, LED_BLUE HIGH;
+   - case -1: Final state not reached, LED_RED HIGH;
+   - case -2: Input sequence not legal, LED_RED blinks 5 times then HIGH;
+*/
+void outUpdate(int state) {
+    switch(state) {
+        case 0:
+            clearAllLed();
+            setLed(LED_GREEN, HIGH);
+            break;
+        case 3:
+            clearAllLed();
+            setLed(LED_BLUE, HIGH);
+            break;
+        case -1:
+            clearAllLed();
+            setLed(LED_RED, HIGH);
+            break;
+        case -2:
+            clearAllLed();
+            blinkLed(LED_RED);
+            break;
+        default:
+            clearAllLed();
+            setLed(LED_YELLOW, HIGH);
+    }
 }
 
 
@@ -96,14 +122,12 @@ int main () {
     st.insert(make_pair(make_pair(1, 'c'), 3));
     st.insert(make_pair(make_pair(2, 'a'), 0));
     st.insert(make_pair(make_pair(2, 'd'), 3));
-    
-    int state = 0;
-    int e;
-    char in;
-    bool lap = false;
 
     init();
-    setLed(LED_GREEN, HIGH);
+    
+    int state = 0;
+    char in;
+    outUpdate(state);
 
     // Getting sequence word from stdin
     string word;
@@ -115,49 +139,41 @@ int main () {
         in = next(word);
         cout << "Next input char is: " << in << "\t" << "Current state is: " << state << endl;
         
-        // If input stream is finished
-        if (in == 'x') {
-            e = -1;
-            goto error;
+        // If input stream is finished exit cycle
+        if (in == '-') {
+            cout << "ERROR!" << endl;
+            cout << "Inputs stream finished - Final state not reached " << endl;
+            cout << "State reached is " << state << endl;
+            state = -1;
+            break;
         }
 
         auto it = st.find(make_pair(state,in));
-        // If input sequence is not legal
+        // If input sequence is not legal exit cycle
         if (it == st.end()) {
-            e = -2;
-            goto error;
+            cout << "ERROR!" << endl;
+            cout << "Input " << in << " not legal for state " << state << endl;
+            state = -2;
+            break;
         }
+        
         state = it->second;
         cout << "State updated to: " << state << endl;
-        
+
         // If final state is reached exit cycle
-        if (state == 3)
+        if (state == 3) {
+            cout << "Input sequence is legal for our Language" << endl;
             break;
-        
-        // If state is updated (and is not error state or final state) set mid-state led indicator
-        if(!lap)
-            setLed(LED_YELLOW, HIGH);
-            lap = true;
+        }
+
+        // Get here if state updated with no errors and final state not reached
+        outUpdate(state);
     }
     
-    // Final state reached - clear mid-state led indicator, set final-state led indicator
-    setLed(LED_YELLOW, LOW);
-    setLed(LED_BLUE, HIGH);
-    cout << "Input sequence is legal for our Language" << endl;
-    return 0;
-    
-    error:
-        switch(e) {
-            case -1:
-                simpleError();
-                cout << "ERROR!" << endl;
-                cout << "Inputs stream finished - Final state not reached " << endl;
-                cout << "State reached is " << state << endl;
-                break;
-            case -2:
-                blinkError();
-                cout << "ERROR!" << endl;
-                cout << "Input " << in << " not legal for state " << state << endl;
-                break;
-        }
+    // Get here if an error occured or final state is reached
+    outUpdate(state);
+    cout << "Program ended, closing in 10sec" << endl;
+    sleep(10);
+    clearAllLed();
+    return state;
 }
